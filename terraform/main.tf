14b2c64664c624369a6738f46c8bbb0af62d2b9e
Mika Kaukolinna <mika.kaukolinna@gofore.com>
2020-06-11 00:07:49 +0300

    Terraform workflow added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,23 @@
-provider "aws" {
+#provider "aws" {
   #profile    = "sandboxtt"
-  region     = "eu-west-1"
+#  region     = "eu-west-1"
+#}
+#resource "aws_s3_bucket" "terraform_state_s3_bucket" {
+#    bucket = "g-cloud-academy-terraform-githup-exercise"
+#}
+
+terraform {
+ backend "remote" {
+   organization = "g-cloud-academy"
+
+   workspaces {
+     name = "g-c-a-terraform"
+   }
+ }
 }
-resource "aws_s3_bucket" "terraform_state_s3_bucket" {
-    bucket = "g-cloud-academy-terraform-githup-exercise"
+
+resource "null_resource" "terraform-github-actions" {
+ triggers = {
+   value = "This resource was created using GitHub Actions!"
+ }
 }
