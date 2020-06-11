38b96055dab1e4c19b908e7e3a4cb3cdbc1d8cfa
Mika Kaukolinna <mika.kaukolinna@gofore.com>
2020-06-11 22:20:48 +0300

    Terraform workflow modified

--- main.tf
+++ main.tf
@@ -2,9 +2,6 @@ provider "aws" {
   #profile    = "sandboxtt"
   region     = "eu-west-1"
 }
-#resource "aws_s3_bucket" "terraform_state_s3_bucket" {
-#    bucket = "g-cloud-academy-terraform-githup-exercise"
-#}
 
 terraform {
  backend "remote" {
@@ -17,5 +14,5 @@ terraform {
 }
 
 resource "aws_s3_bucket" "terraform_s3_bucket" {
-    bucket = "g-cloud-academy-terraform-github-70"
+    bucket = "g-cloud-academy-terraform-github-71"
 }
